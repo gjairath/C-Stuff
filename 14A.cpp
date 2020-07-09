803c8f270565607d9f5822c06145c3e25a4ec7c9
ajay jairath <gjairath@purdue.edu>
2020-07-08 23:01:32 -0400

    Sick hack - 157A

--- 14A.cpp
+++ 14A.cpp
@@ -4,37 +4,43 @@
 #include <cstdlib>
 #include <string>
 using namespace std;
-	
+
 int main() {
 	
-	int rows, cols;
-	cin >> rows >> cols;
-	char a[101][101];
+	int width;
+	cin >> width;
+	int a[width][width];
+	int row_sum[width];
+	int col_sum[width];
 	
-	int x = 1e8, xm = 0, y = 1e8, ym = 0;
+	int row_sums = 0;
+	int col_sums = 0;
 	
-	for (int i = 1; i <= rows ; i++) {
-		for (int j = 1; j <= cols; j++) {
+	for (int i = 0; i < width; i++) {
+		for (int j = 0; j < width; j++) {
 			cin >> a[i][j];
-			
-			if (a[i][j] == '*') {
-				// we want to store the 4 parameters
-				x = min(x,i); // For the first example this should be 2.
-				y = min(y,j); // for the first example, this should be 3
-				
-				xm = max(xm, i); // this should be 6
-				ym = max(ym, j); // this should be 5
-				
+		}
+	}
+	
+	for (int i = 0; i < width; i++) {
+		for (int j = 0; j < width; j++) {
+			row_sums += a[i][j]; 
+			col_sums += a[j][i];
+		}
+		row_sum[i] = row_sums;
+		row_sums = 0;
+		col_sum[i] = col_sums;
+		col_sums = 0;
+	}
+	int counter = 0;
+	for (int i = 0; i < width; i++) {
+		for (int j = 0; j < width; j++) {
+			if (col_sum[i] > row_sum[j]) {
+				counter++;
 			}
 		}
 	}
-	
-	for (int i = x; i <= xm; i++) {
-		for (int j = y; j <= ym; j++) {
-			cout << a[i][j];
-		}
-		cout << '\n';
-	}
+	cout << counter;
     return 0;
 	
 }
